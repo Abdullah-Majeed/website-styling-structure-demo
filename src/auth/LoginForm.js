e7ce60eea641de40d449f238c3b000158434d55a
Abdullah <abdullahmajeed969@gmail.com>
2022-11-05 17:17:38 +0500

    sass working

---
+++ src/auth/LoginForm.js
@@ -0,0 +1,10 @@
+import './auth.scss'
+import './auth.css'
+import React from 'react'
+
+export const LoginForm = () => {
+  return (
+    <div className='auth_login_text_color auth_login_text_font'>LoginForm</div>
+  )
+}
+
